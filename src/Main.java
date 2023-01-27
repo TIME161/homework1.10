cb1b993531389ff09b80968052cf658df50e23c5
TIME <opout777@yandex.ru>
2023-01-27 13:16:16 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -13,5 +14,11 @@ public class Main {
 
         System.out.println("Задача 2");
         System.out.println(fullName.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println(fullName);
     }
 }
